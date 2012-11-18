3cc752fb4a99943453525d53531cad0177588aba
phillipkregg <phillipkregg@gmail.com>
2012-11-18 17:34:23 -0600

    Initial commit - Day 4

---
+++ jobeet/apps/frontend/modules/job/templates/indexSuccess.php
@@ -0,0 +1,19 @@
+<!-- apps/frontend/modules/job/templates/indexSuccess.php -->
+<?php use_stylesheet('jobs.css') ?>
+ 
+<div id="jobs">
+  <table class="jobs">
+    <?php foreach ($JobeetJobs as $i => $job): ?>
+      <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
+        <td class="location"><?php echo $job->getLocation() ?></td>
+        <td class="position">
+          <a href="<?php echo url_for('job_show_user', $job)  ?>">
+            <?php echo $job->getPosition() ?>
+          </a>
+        </td>
+        <td class="company"><?php echo $job->getCompany() ?></td>
+        
+      </tr>
+    <?php endforeach ?>
+  </table>
+</div>
